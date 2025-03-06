aabdb2646837aaadfa276148e9ab15ab2ad65aa4
lahiru59 <lahiru.bm08@gmail.com>
2025-03-06 10:03:26 +0530

    Add files via upload

---
+++ lab3.1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+    int main(){
+        int studentID = 54321;
+        float GPA = 3.90;
+        char grade = 'A';
+        char val = '+';
+
+
+        printf("Updated studentID: %d\n",studentID);
+        printf("Updated GPA: %f\n",GPA);
+        printf("Updated grade: %c%c\n",grade,val);
+
+        return 0;
+     }
---
+++ lab3.2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+#define speed_of_light 299792458
+#define GRAVITY 9.81
+#define PLANCK_CONSTANT 6.626e-34
+
+ const float BOLTZMANN_CONSTANT = 1.38e-23;
+ const int ABSOLUTE_ZERO = -273;
+
+ printf("The value of speed of light: %d\n",speed_of_light);
+ printf("The value of GRAVITY: %f\n",GRAVITY);
+ printf("The value of BOLTZMANN CONSTANT: %f\n",BOLTZMANN_CONSTANT);
+ printf("The value of ABSOLUTE_ZERO: %d\n",ABSOLUTE_ZERO);
+
+ return 0;
+}
